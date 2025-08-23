f45a3dd914884c8f042283f808e3dd40cd04fa91
imamhossainroni <imamhossainroni95@gmail.com>
2025-08-24 01:02:50 +0200

    notes read and delete functionality added.

--- main.py
+++ main.py
@@ -28,3 +28,62 @@ def add_note(message: str) -> str:
     with open(NOTES_FILE, "a") as f:
         f.write(message + "\n")
     return "Notes Saved!"
+
+
+@mcp.tool()
+def read_latest_notes():
+    """
+    Get the most recently added note from the sticky note file.
+
+    Returns:
+       str: The last note entry. If no notes exist, a default message is returned.
+    """
+    ensure_file()
+    with open(NOTES_FILE, "r") as f:
+        lines = f.readlines()
+    return lines[-1].strip() if lines else "No notes yet."
+
+
+@mcp.tool()
+def read_all_notes():
+    """
+    Get the all added note from the sticky note file.
+
+    Returns:
+       str: The all note entry. If no notes exist, a default message is returned.
+    """
+    ensure_file()
+    with open(NOTES_FILE, "r") as f:
+        lines = f.readlines()
+    if not lines:
+        return "No notes yet."
+    notes = ""
+    for note in lines:
+        notes += "=>" + note.strip() + "\n"
+
+    return notes
+
+
+@mcp.tool()
+def delete_note(total_num: int) -> bool:
+    """
+    Get the all added note from the sticky note file.
+
+    Returns:
+        bool: returns True if the notes are deleted else False.
+    """
+    ensure_file()
+    with open(NOTES_FILE, 'r') as file:
+        notes = file.readlines()
+
+    if total_num > len(notes):
+        print(f"Only {len(notes)} notes available, cannot delete {total_num}")
+        return False
+    remaining_notes = notes[:-total_num]
+
+    # Write remaining notes back to file
+    with open(NOTES_FILE, 'w') as file:
+        file.writelines(remaining_notes)
+
+    print(f"Successfully deleted {total_num} note(s)")
+    return True
